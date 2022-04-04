c5c01d04f35f6b44c32d5098c7083660d052b95c
ARez2 <dark.gaming@fantasymail.de>
2022-04-04 20:47:32 +0200

    Code beautifying (Static typing, Comments)

--- Scenes/Player/Player.gd
+++ Scenes/Player/Player.gd
@@ -1,23 +1,35 @@
 extends KinematicBody2D
 class_name Player
 
-export var Sensitivity = 10 # 65 for 1:1 scale
-export var MaxSpeed = 800
+# ====================== VARIABLES ======================
+# ----------- EXPORTS -----------
+export var Sensitivity := 10 # 65 for 1:1 scale
+export var MaxSpeed := 800
 export var Gravity := 10
 
-var pressed_down = false
+# ----------- NORMAL -----------
+var pressed_down := false
 var mouse_movement := Vector2()
 var initial_pos := Vector2()
+var velocity := Vector2()
+var playersize := 8
+var tilemap_size := 16
+var min_vel_diff := 50
+var last_side_hit = 0.0
 
+# ----------- ONREADY -----------
 onready var camera : Camera2D = get_tree().get_nodes_in_group("cam")[0]
 onready var camera_target = get_tree().get_nodes_in_group("cam_target")[0]
-onready var screen_size = Vector2(ProjectSettings.get_setting("display/window/size/width"), ProjectSettings.get_setting("display/window/size/height"))
-onready var crosshair_outer = get_tree().get_nodes_in_group("crosshair")[0].get_child(0)
-onready var crosshair_inner = get_tree().get_nodes_in_group("crosshair")[0].get_child(1)
-var playersize = 8
-var tilemap_size = 16
+onready var screen_size := Vector2(ProjectSettings.get_setting("display/window/size/width"), ProjectSettings.get_setting("display/window/size/height"))
+onready var crosshair_outer : Sprite = get_tree().get_nodes_in_group("crosshair")[0].get_child(0)
+onready var crosshair_inner : Sprite = get_tree().get_nodes_in_group("crosshair")[0].get_child(1)
+onready var floor_check : RayCast2D = $FloorCheck
 
-var velocity := Vector2()
+
+# ====================== MAIN FUNCTIONS ======================
+
+func _ready() -> void:
+	pass
 
 
 func _input(event: InputEvent) -> void:
@@ -32,12 +44,11 @@ func _input(event: InputEvent) -> void:
 		mouse_movement = movement.relative * Sensitivity
 
 
-# The higher the velocity, the more blocks it will affect
-
-
-var last_side_hit = 0.0
 func _physics_process(delta: float) -> void:
 	last_side_hit += delta
+	var on_floor := floor_check.is_colliding()
+	
+	# ----------- VELOCITY CALC -----------
 	var v_len := 0.0
 	if pressed_down:
 		var mouse_pos = get_viewport().get_mouse_position()
@@ -52,37 +63,55 @@ func _physics_process(delta: float) -> void:
 		mouse_movement = Vector2.ZERO
 	v_len = velocity.length()
 	
+	
+	# ----------- VISUAL STUFF -----------
 	crosshair_inner.visible = pressed_down
 	crosshair_outer.visible = pressed_down
-
-	velocity = move_and_slide(velocity + mouse_movement)# - mouse_movement
 	
-	var min_diff = 50
-	if get_slide_count() > 0 \
-		and v_len - velocity.length() > min_diff \
-		and last_side_hit > 0.2 \
-		and v_len > 100:
-		last_side_hit = 0.0
-		$Dust.look_at($Dust.global_position + get_slide_collision(0).normal)
-		$Dust.restart()
-		var tilemap = get_slide_collision(0).collider as TileMap
-		if tilemap is DestructibleTilemap:
-			var pos = tilemap.world_to_map(global_position)
-			var smash_radius = (MaxSpeed / min_diff) - (MaxSpeed / v_len)
-			smash_radius /= 5
-			smash_radius = int(smash_radius)
-			for x in range(-smash_radius, smash_radius):
-				for y in range(-smash_radius, smash_radius):
-					RoomsManager.damage_cell(pos + Vector2(x, y), v_len)
-			get_tree().get_nodes_in_group("cam")[0].add_trauma(.2)
+	if on_floor:
+		if v_len > 2:
+			$Sprite.play("run")
+		else:
+			$Sprite.play("idle")
+	else:
+		$Sprite.play("jump")
 	
 	if velocity != Vector2.ZERO:
 		$Sprite.flip_h = velocity.x < 0
 	
+	
+	# ----------- MOVE -----------
+	velocity = move_and_slide(velocity + mouse_movement)# - mouse_movement
+	
+	
+	# ----------- POST MOVE -----------
+	if get_slide_count() > 0 \
+	  and v_len - velocity.length() > min_vel_diff \
+	  and last_side_hit > 0.2 \
+	  and v_len > 100:
+		smash(v_len)
 	mouse_movement = Vector2.ZERO
 
 
-func take_damage(amt):
+
+# ====================== FUNCTIONS ======================
+func smash(v_len) -> void:
+	last_side_hit = 0.0
+	$Dust.look_at($Dust.global_position + get_slide_collision(0).normal)
+	$Dust.restart()
+	var tilemap = get_slide_collision(0).collider as TileMap
+	if tilemap is DestructibleTilemap:
+		var pos = tilemap.world_to_map(global_position)
+		var smash_radius = (MaxSpeed / min_vel_diff) - (MaxSpeed / v_len)
+		smash_radius /= 5
+		smash_radius = int(smash_radius)
+		for x in range(-smash_radius, smash_radius):
+			for y in range(-smash_radius, smash_radius):
+				RoomsManager.damage_cell(pos + Vector2(x, y), v_len)
+		camera.add_trauma(.2)
+
+
+func take_damage(amt) -> void:
 	print("Dmg: ", amt)
 
 
